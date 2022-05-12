4546c44773877f3c34af06a795373ea9160b3140
Rustam Mustafin <rustammuus@mail.ru>
2022-05-12 17:13:15 +0300

    Initial Commit1

--- src/main/java/ru/netology/sqr/Main.java
+++ src/main/java/ru/netology/sqr/Main.java
@@ -3,7 +3,7 @@ package ru.netology.sqr;
 public class Main {
     public static void main(String[] args) {
         SQRService service = new SQRService();
-        int res = service.squaring(50, 90);
+        int res = service.squaring(200, 300);
         System.out.println(res);
     }
 }
